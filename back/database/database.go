342c1f51dba8e7ff9f1b9f80e77415e98908bfbf
Ivan Belov <dionis.krasava@gmail.com>
2025-09-29 17:20:18 +0700

    +

--- back/database/database.go
+++ back/database/database.go
@@ -11,7 +11,8 @@ import (
 var DB *gorm.DB
 
 func Connect() {
-	dsn := "host=localhost user=postgres password=yourpassword dbname=myapp port=5432 sslmode=disable"
+	//dsn := "host=localhost user=postgres password=yourpassword dbname=myapp port=5432 sslmode=disable"
+	dsn := "host=localhost user=myuser password=mypassword dbname=myapp port=5432 sslmode=disable"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to database:", err)
